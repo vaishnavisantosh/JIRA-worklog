900bd80604e389365360f8b127bc5a90fa1d2206
vaishnavisantosh <vaishnavijawanjal29@gmail.com>
2019-12-18 20:15:15 +0530

    done with filter section

--- jiraworklog/src/Component/User/User.js
+++ jiraworklog/src/Component/User/User.js
@@ -8,7 +8,7 @@ const User = (props) => {
     return (
         <tr>
             <td><img style={{width:'35px'}} src= {props.avatarUrls}/> </td>
-            <td style={{fontSize:'15px',fontWeight:'bold'}}>{props.name}</td>
+            <td style={{fontSize:'15px',fontWeight:'bold',Width:'200px'}}>{props.name}</td>
             {
                 props.time.map(i=><td key={i}>{i}</td>)
             }
--- jiraworklog/src/Container/Timesheet/Timesheet.js
+++ jiraworklog/src/Container/Timesheet/Timesheet.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
+import User from '../../Component/User/User';
+import DateRangePicker from '@wojtekmaj/react-daterange-picker'
+import 'bootstrap/dist/css/bootstrap.css';
+import { Search, Grid, Header, Segment } from 'semantic-ui-react'
+import _ from 'lodash';
+
+// import moment from "moment";
 // import {} from 'semantic-ui-react';
 //  import DateRangePicker from '../../Component/DateRangePicker/DateRangePicker';
 // import { DateRangePicker } from 'react-date-range';
-
-import { Table } from 'react-bootstrap';
-import User from '../../Component/User/User';
 // import {DateRangePicker} from 'rsuite';
 // import 'rsuite/lib/styles/index.less'; 
 // or 
 // import 'rsuite/dist/styles/rsuite-default.css';
-import DateRangePicker from '@wojtekmaj/react-daterange-picker'
-import 'bootstrap/dist/css/bootstrap.css';
-// import moment from "moment";
+
+
 const base64 = require('base-64');
 const moment = require('moment');
 
-
+ const initialState = { isLoading: false, results: [], value: '' }
+ var date = new Date();
+ var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+ var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 class Timesheet extends Component {
 
     state = {
         user: [],
-        date: [new Date(),new Date()]
+        date: [firstDay, lastDay],
+        isLoading: false, 
+        results: [],
+         value: ''
     }
 
 
@@ -32,58 +42,100 @@ class Timesheet extends Component {
         headers.append("Content-Type", "application/json");
         headers.append("Accept", "application/json");
         headers.append('Authorization', 'Basic ' + base64.encode(`${email}:${api}`));
-        let arr = [], totalIssues, issueKeys = [];
-        fetch(`${url}/rest/api/2/user/assignable/search?project=REAC`, { method: 'GET', headers: headers })
+        let arr = [], totalIssues, issueKeys = [], projectKey;
+
+        fetch(`${url}/rest/api/2/project`, { method: 'GET', headers: headers })
             .then(res => res.json())
             .then(res => {
-                for (let key in res) {
-                    arr.push({
-                        id: res[key].accountId,
-                        avatarUrls: Object.values(res[key].avatarUrls)[3],
-                        name: res[key].displayName,
-                        worklog: []
-                        // datea:this.state.dateArray
-                    });
-
-                }
-                console.log("response", res);
-                console.log("user array", arr)
-                this.setState({ user: arr })
-                console.log("state", this.state.user);
+                projectKey = res[0].key
+                console.log('it is 1st api which gives all project keys', projectKey)
 
             }).then(() => {
 
-                fetch(`${url}/rest/api/2/search?jql=project=REAC&fields=issue,name&startAt=0&maxResults=8000 `, { method: 'GET', headers: headers })
+                fetch(`${url}/rest/api/2/user/assignable/search?project=${projectKey}`, { method: 'GET', headers: headers })
                     .then(res => res.json())
                     .then(res => {
-                        // totalIssues = res;
-                        for (let issuekey in res.issues) {
-                            issueKeys.push(res.issues[issuekey].key);
-                        }
-                        // console.log('isuessssssss',totalIssues);
-                        // console.log('res.issue',res.issues)
-                        console.log("key arrrayy", issueKeys)
-                    }).then(() => {
-                        issueKeys.map(i => {
-                            fetch(`${url}/rest/api/3/issue/${i}/worklog`, { method: 'GET', headers: headers })
-                                .then(res => res.json())
-                                .then(res => {
-                                    for (let log in res.worklogs) {
-                                        res.worklogs.map(i => {
-                                            if (i.author.key === arr[1].id) {
+                        for (let key in res) {
+                            arr.push({
+                                id: res[key].accountId,
+                                avatarUrls: Object.values(res[key].avatarUrls)[3],
+                                name: res[key].displayName,
+                                worklog: []
+                                // datea:this.state.dateArray
+                            });
 
-                                                arr[0].worklog.push((i.timeSpentSeconds) / 3600)
+                        }
+                        console.log("response", res);
+                        console.log("user array", arr)
+                        this.setState({ user: arr })
+                        console.log("state", this.state.user);
+
+                    }).then(() => {
+
+                        fetch(`${url}/rest/api/2/search?jql=project=${projectKey}&fields=issue,name&startAt=0&maxResults=8000 `, { method: 'GET', headers: headers })
+                            .then(res => res.json())
+                            .then(res => {
+                                // totalIssues = res;
+                                for (let issuekey in res.issues) {
+                                    issueKeys.push(res.issues[issuekey].key);
+                                }
+                                // console.log('isuessssssss',totalIssues);
+                                // console.log('res.issue',res.issues)
+                                console.log("key arrrayy", issueKeys)
+                                console.log('issuessssssssssssssssssssssss', res.issues)
+                            }).then(() => {
+                                issueKeys.map(i => {
+                                    fetch(`${url}/rest/api/3/issue/${i}/worklog`, { method: 'GET', headers: headers })
+                                        .then(res => res.json())
+                                        .then(res => {
+                                            for (let log in res.worklogs) {
+                                                res.worklogs.map(i => {
+                                                    if (i.author.key === arr[1].id) {
+
+                                                        arr[0].worklog.push((i.timeSpentSeconds) / 3600)
+                                                    }
+                                                })
                                             }
+                                            console.log('updated arrayyyyyyyyyyyyy', arr);
+                                            console.log('its api call for iterating all issues', res)
                                         })
-                                    }
-                                    console.log('updated arrayyyyyyyyyyyyy', arr);
-                                    console.log('its api call for iterating all issues', res)
-                                })
-                        });
+                                });
+                            });
                     });
             });
 
     }
+
+   
+
+    handleResultSelect = (e, { result }) => {
+        console.log('single res', result);
+        this.setState({ value: result.name })
+    }
+
+    handleSearchChange = (e, { value }) => {
+        this.setState({ isLoading: true, value })
+
+        setTimeout(() => {
+            let data=this.state.user;
+
+            if (this.state.value.length < 1) return this.setState({users:data})
+            const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+            const isMatch = (data) => re.test(data.name)
+            const users = this.state.user.map(i => {
+                return {...i, title: i.name, image: i.avatarUrls};
+            })
+            this.setState({
+                isLoading: false,
+                results: _.filter(users, isMatch),
+            })
+            this.setState({user:this.state.results})
+            console.log('results -- ', _.filter(data, isMatch));
+        }, 300)
+       
+       
+    }
+
 
     getDateArray = (start, end) => {
         var dateArray = new Array();
@@ -97,7 +149,8 @@ class Timesheet extends Component {
     }
 
     handleDate = (date) => {
-        this.setState({ date: date })
+        console.log('new date =', date);
+        this.setState({ date })
         // console.log(this.state.date[1]);
 
         // this.getDateArray(this.state.date[0],this.state.date[1])
@@ -111,26 +164,58 @@ class Timesheet extends Component {
         console.log("inside renderrrrrrr", this.state.user);
         let user;
         console.log('date stateeeeeeeeeeeeee', this.state.date);
-        let dateArr = this.getDateArray(this.state.date[0], this.state.date[1]);
+        // const datesArr = JSON.parse(JSON.stringify(this.state.date))
+        let dateArr = this.getDateArray(new Date(this.state.date[0]), new Date(this.state.date[1]));
 
         console.log('dateArrrrrrrrrrrrrrrrrrrrrrrrrrrr', dateArr)
+        const { isLoading, value, results } = this.state
 
         return (
             <>
-
-
-                <DateRangePicker
+                 <DateRangePicker 
                     onChange={this.handleDate}
                     value={this.state.date}
+                    format="y-MM-dd"
                 />
+
+                <Grid style={{display:'inline'}}>
+                    <Grid.Column width={6}>
+                        <Search
+                            aligned='right'
+                            loading={isLoading}
+                            name="name"
+                            onResultSelect={this.handleResultSelect}
+                            onSearchChange={_.debounce(this.handleSearchChange, 500, {
+                                leading: true,
+                            })}
+                            results={this.state.results}
+                            value={value}
+                            {...this.props}
+                        />
+                    </Grid.Column>
+                    <Grid.Column width={10}>
+                        {/* <Segment>
+                            <Header>State</Header>
+                            <pre style={{ overflowX: 'auto' }}>
+                                {JSON.stringify(this.state, null, 2)}
+                            </pre>
+                            <Header>Options</Header>
+                            <pre style={{ overflowX: 'auto' }}>
+                                {JSON.stringify(this.state.user, null, 2)}
+                            </pre>
+                        </Segment> */}
+                    </Grid.Column>
+                </Grid>
+
+               
 
                 <Table responsive>
                     <thead>
                         <tr>
-                            <th style={{ width: '75px' }}>Users</th>
-                            <th style={{ width: '100px' }}></th>
+                            <th style={{ width: '22px' }}>Users</th>
+                            <th > </th>
                             {
-                                dateArr.map(i => <th key={i}>{moment(i).format(' D ddd')}</th>)
+                                dateArr.map(i => <th  key={i}>{moment(i).format(' D ddd')}</th>)
                             }
 
                         </tr>
