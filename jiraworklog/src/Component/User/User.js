1c17a6b708982b7ace7e00f7b76da96154ee7622
vaishnavisantosh <vaishnavijawanjal29@gmail.com>
2019-12-23 20:09:35 +0530

    done with modal handaling

--- jiraworklog/src/Component/User/User.js
+++ jiraworklog/src/Component/User/User.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 // import { Button, Table,Modal } from 'react-bootstrap';
 // import 'bootstrap/dist/css/bootstrap.css';
 import { Button, Header, Icon, Modal } from 'semantic-ui-react'
@@ -9,41 +9,44 @@ const base64 = require('base-64');
 
 
 const User = (props) => {
-   const modalFuction=()=>{
-        console.log("model function called")
-        alert('model callled');
-    }
-  
-    const [show, setShow] = useState(false);
+  const modalFuction = () => {
+    console.log("model function called")
+    alert('model callled');
+  }
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    return (
-        <>
-        
-        <tr>
-            <td><img style={{width:'35px'}} src= {props.avatarUrls}/> </td>
-            <td style={{fontSize:'15px',fontWeight:'bold',Width:'800px'}}>{props.name}</td>
-            {
-                props.time.map(i=><td style={{color:'blue',cursor:'pointer'}} onClick={handleShow}>{`${i}h`}</td>)
-            }
-            
-        </tr>
+  const [show, setShow] = useState(false);
 
-        <Modal show={show} onHide={handleClose} animation={false}>
-        <Modal.Header closeButton>
-          <Modal.Title>WorkLog</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-         
-        </Modal.Footer>
-      </Modal>
-        </>
-    );
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+
+      <tr>
+        {/* <Modal trigger={<Button>Show Modal</Button>}> */}
+          <Modal.Header>Select a Photo</Modal.Header>
+          <Modal.Content image>
+            <img wrapped size='medium' src={props.avatarUrls} />
+            <Modal.Description>
+              <Header>Default Profile Image</Header>
+              <p>
+                We've found the following gravatar image associated with your e-mail
+                address.
+        </p>
+              <p>Is it okay to use this photo?</p>
+            </Modal.Description>
+          </Modal.Content>
+        {/* </Modal> */}
+        <td><img style={{ width: '35px' }} src={props.avatarUrls} /> </td>
+        <td style={{ fontSize: '15px', fontWeight: 'bold', Width: '800px' }}>{props.name}</td>
+        {
+          props.time.map(i =><Modal trigger= {<td style={{ color: 'blue', cursor: 'pointer' }} onClick={handleShow}>{`${i}h`}</td>}></Modal>)
+        }
+
+      </tr>
+
+
+    </>
+  );
 
 
 }
